7cb08f876c626b1d9d541e10c0c6ebe59b11060f
Agustin Freda <agustinfreda2004@gmail.com>
2025-03-04 13:46:57 -0300

    feat: fetch_json

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// URL_ITALIA := "https://coalplus.volantinointerattivo.net/"
+
+	// URL_API_ITALIA := "https://view.inpublish.it/kUHRuZraa8oGZGvq/04-coal-market-city-25/config/publication.json?v=0"
+
+	// api.ConsumarApi(URL_API_ITALIA)
+}
