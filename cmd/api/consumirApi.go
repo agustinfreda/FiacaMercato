503334eec5b3af916b6a4ea750c4751ea88207c5
Agustin Freda <agustinfreda2004@gmail.com>
2025-03-04 14:13:42 -0300

    consumirApi y main

--- cmd/api/consumirApi.go
+++ cmd/api/consumirApi.go
@@ -1,11 +1,66 @@
 package api
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
+
+func ConsumarApi(URL string) {
+	data, err := fetchJSON(URL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	products, err := extractInteractivities(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err := writeCSV("productos.csv", products); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Archivo productos.csv creado correctamente.")
+}
+func writeCSV(filename string, products map[string]interface{}) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("error al crear el archivo CSV: %v", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	headers := []string{"id", "nombre", "precio_oferta", "precio"}
+	if err := writer.Write(headers); err != nil {
+		return fmt.Errorf("error al escribir encabezado en CSV: %v", err)
+	}
+
+	for id, rawProduct := range products {
+		product, ok := rawProduct.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		nombre := fmt.Sprintf("%v", product["field1"])
+		precio_oferta := fmt.Sprintf("%v", product["offer_price"])
+		precio := fmt.Sprintf("%v", product["price"])
+
+		if err := writer.Write([]string{id, nombre, precio_oferta, precio}); err != nil {
+			return fmt.Errorf("error al escribir en CSV: %v", err)
+		}
+	}
+
+	return nil
+}
 
 func extractInteractivities(data map[string]interface{}) (map[string]interface{}, error) {
 	clientRaw, ok := data["client"]
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
+import "github.com/agustinfreda/FiacaMercato/cmd/api"
+
 func main() {
 	// URL_ITALIA := "https://coalplus.volantinointerattivo.net/"
 
-	// URL_API_ITALIA := "https://view.inpublish.it/kUHRuZraa8oGZGvq/04-coal-market-city-25/config/publication.json?v=0"
+	URL_API_ITALIA := "https://view.inpublish.it/kUHRuZraa8oGZGvq/04-coal-market-city-25/config/publication.json?v=0"
 
-	// api.ConsumarApi(URL_API_ITALIA)
+	api.ConsumarApi(URL_API_ITALIA)
 }
